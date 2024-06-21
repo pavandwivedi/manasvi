06a10e560ea53a7e7676311637ffbba08403e37a
pavandwivedi <pavandwivedi35@gmail.com>
2024-06-21 16:26:59 +0530

    updated

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRouter from "./routes/userRoutes.js";
+import cors from "cors";
 dotenv.config();
 connectDB();
 const app = express();
+app.use(cors());
 app.use(express.json());
+
  app.use('/user',userRouter);
 const port = process.env.PORT || 3000;
 
